0d9cfd45ff77b8dcc4448e59241326560e594ed4
Vygovskii Oleg <vygovskii@yandex.ru>
2019-09-25 16:26:09 +0500

    first launch

--- client/chat-client/src/app/shared/material/material.module.ts
+++ client/chat-client/src/app/shared/material/material.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialog,
+  MatDialogModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  providers: [
+    MatDialog
   ]
 })
+
+
 export class MaterialModule { }

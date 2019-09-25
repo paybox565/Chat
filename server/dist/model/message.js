7287170a22bdddef6c47d0a6a8f8664832a0ad1a
Vygovskii Oleg <vygovskii@yandex.ru>
2019-09-25 17:10:30 +0500

    server start task

---
+++ server/dist/model/message.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Message = /** @class */ (function () {
+    function Message(from, content) {
+        this.from = from;
+        this.content = content;
+    }
+    return Message;
+}());
+exports.Message = Message;
